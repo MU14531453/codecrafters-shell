79dbe1cbd9d1321b50455d316e4a862ef7bdbab9
MU14531453 <michal1442442@gmail.com>
2025-05-09 19:34:41 +0200

    [any message]

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 import sys
+import os
 import shutil
+import random
+import subprocess
 
 def get_command_name(command, command_list):
 
@@ -7,7 +10,7 @@ def get_command_name(command, command_list):
 
 def main():
 
-    command_list = ['exit', 'echo', 'type', 'PATH']
+    command_list = ['exit', 'echo', 'type', 'PATH', 'pwd', 'cd']
     PATH = []
     
     sys.stdout.write('$ ')
@@ -18,32 +21,47 @@ def main():
         command = input()
 
         command_full = command.split(' ')
-        identifier  = command_full[0]
+        identifier = command_full[0]            
 
-        if identifier not in command_list:
-            sys.stdout.write(f'{command}: command not found')
-        else:
-            match identifier:
+        match identifier:
 
-                case 'exit':
-                    exit(int(command_full[1]))
+            case 'exit':
+                exit(int(command_full[1]))
 
-                case 'echo':
-                    sys.stdout.write(command_full[1])
+            case 'echo':
+                sys.stdout.write(command_full[1])
 
-                case 'type':
-                    if command_full[1].strip() in command_list:
-                        sys.stdout.write(f'{command_full[1]} is a shell builtin')
-                    elif PATH and PATH in shutil.which(command_full[1]):
-                        sys.stdout.write(f'{command_full[1]} is {PATH}')
-                    else:
-                        sys.stdout.write(f'{command_full[1]}: not found')
+            case 'type':
+                if command_full[1].strip() in command_list:
+                    sys.stdout.write(f'{command_full[1]} is a shell builtin')
+                elif PATH and PATH in shutil.which(command_full[1]):
+                    sys.stdout.write(f'{command_full[1]} is {PATH}')
+                else:
+                    sys.stdout.write(f'{command_full[1]}: not found')
+            
+            case 'PATH':
+                path = []
+
+                command_full[1] = command_full[1][:-5]
                 
-                case 'PATH':
-                    PATH.append(command_full[1])
+                for filepath in command_full[1].split(':'):
+                    PATH.append(filepath)
 
-                case default:
-                    return -1
+            case 'pwd':
+                sys.stdout.write(os.getcwd())
+
+            case 'cd':
+                pass
+
+            case default:
+                if command in shutil.which(PATH):
+                    subprocess.call(command)
+                    #ret = subprocess.run(command)
+                    #sys.stdout.write(ret.stdout)
+                else:
+                    sys.stdout.write(f'{command}: command not found')
+                
+
 
         sys.stdout.write('\n$ ')
 
