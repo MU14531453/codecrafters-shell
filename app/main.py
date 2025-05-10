945a52dbfdb081d4e89a31c4a4839251f1516952
MU14531453 <michal1442442@gmail.com>
2025-05-10 18:00:17 +0200

    [any message]

--- app/main.py
+++ app/main.py
@@ -114,7 +114,7 @@ def main():
             case 'cat':
                 file_list = parser(command_full[1], as_cat = True, as_list = True)
                 for f in file_list:
-                    subprocess.run(identifier + ' ' + f)
+                    subprocess.run('gc ' + f)
 
             case default:
                 print(identifier)
