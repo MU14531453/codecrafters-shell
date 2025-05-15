fd103745d412fcfbedbf9c6a356a0d6e33f199b4
MU14531453 <michal1442442@gmail.com>
2025-05-15 23:47:58 +0200

    [any message]

--- app/main.py
+++ app/main.py
@@ -118,6 +118,7 @@ def main():
             case 'echo':
                 
                 if output_file:
+                    print('tutaj')
                     write_to(output_file, command_full[1])
                 else:
                     print(command_full[1])
