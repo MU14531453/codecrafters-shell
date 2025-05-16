687aeff0badae8090c30fcb2350c905bf9159b0f
MU14531453 <michal1442442@gmail.com>
2025-05-17 00:11:32 +0200

    [any message]

--- app/main.py
+++ app/main.py
@@ -170,7 +170,7 @@ def main():
                     subprocess.run([command], shell = True)
                 else:
                     
-                    temp_writer = subprocess.run([command], shell = True, stdout = False)
+                    temp_writer = subprocess.run([command], shell = True, stdout = None)
                     
                     write_to(output_file, temp_writer)
                 pass
