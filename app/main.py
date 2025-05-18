ecb3b0243a27a42c22c689ae381de361b264ae9c
MU14531453 <michal1442442@gmail.com>
2025-05-18 23:40:38 +0200

    [any message]

--- app/main.py
+++ app/main.py
@@ -184,7 +184,6 @@ def main():
                 if not output_file:
                     subprocess.run([command], shell = True)
                 else:
-                    print('aaaaa;', command_full)
                     write_to(output_file, subprocess.run([' '.join(command_full)], shell = True))
                     output_file = None
                 pass
