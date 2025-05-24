4038ebbd55dd920da5eda25da1f2ffceac1f8bfb
MU14531453 <michal1442442@gmail.com>
2025-05-24 19:57:47 +0200

    [any message]

--- app/main.py
+++ app/main.py
@@ -104,7 +104,7 @@ def std_newline(command):
 
     t = subprocess.run(command, shell = True, capture_output = True)
 
-    sys.stdout.write(f'{str(t.stdout)}\n')
+    sys.stdout.write(f'{str(t.stdout.decode(encoding = 'utf-8'))}\n')
 
     return None
 
