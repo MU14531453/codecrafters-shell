a9529063538ef3911b21e5c67d9a5f50e56bf84f
MU14531453 <michal1442442@gmail.com>
2025-05-24 18:48:54 +0200

    [any message]

--- app/main.py
+++ app/main.py
@@ -167,6 +167,7 @@ def main():
                         res = subprocess.run(command_foo, shell = True)
                     else:
                         res = subprocess.run(' '.join(command_full), shell = True)
+                        print('$ ', end = '')
                 else:
                     print(f'{command}: command not found')
                 
