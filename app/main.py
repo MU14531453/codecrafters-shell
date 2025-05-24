8d82054249d62a68d7e599e951d695c1e90f4a39
MU14531453 <michal1442442@gmail.com>
2025-05-24 17:52:22 +0200

    [any message]

--- app/main.py
+++ app/main.py
@@ -199,7 +199,7 @@ def main():
                 if identifier := shutil.which(identifier if identifier else ''):
                     
                     if output_file is not None:
-                        res = subprocess.run(' '.join(command_full), shell = True, stdout = open(output_file))
+                        res = subprocess.run(' '.join(command_full), shell = True, stdout = open(output_file, 'w+'))
                         #write_to(output_file, res)
                     else:
                         res = subprocess.run(' '.join(command_full), shell = True)
