529fb030bcbff91bf8057b5aafb35e3348a6ca40
MU14531453 <michal1442442@gmail.com>
2025-05-27 21:51:56 +0200

    [any message]

--- app/main.py
+++ app/main.py
@@ -209,7 +209,7 @@ def main():
             
             case default:
                 if identifier := shutil.which(identifier if identifier else ''):
-                    subprocess.run(command_foo, shell = True, stderr = subprocess.DEVNULL)
+                    subprocess.run(command_foo, shell = True, stderr = sys.stdout)
                 else:
                     print(f'{command}: command not found')
 
