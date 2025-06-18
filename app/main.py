6afa6574dae1699b35bf354c5f69ff4d2fc25054
MU14531453 <michal1442442@gmail.com>
2025-06-19 01:06:59 +0200

    [any message]

--- app/main.py
+++ app/main.py
@@ -161,7 +161,7 @@ def main():
                 history_pointer = len(history_list)
             else:
                 history_pointer = max(0, history_pointer - 1)
-            print(f'$ {history_list[history_pointer]}\n$ ')
+            print(f'{history_list[history_pointer]}\n')
             continue
 
         command_foo = copy(command)
