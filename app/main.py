b2223c08ab49f477d0a7aa96d65364f1a80a4c4b
MU14531453 <michal1442442@gmail.com>
2025-06-19 00:30:43 +0200

    [any message]

--- app/main.py
+++ app/main.py
@@ -143,10 +143,10 @@ def main():
     dynamic_commands = sorted(dynamic_commands)
     completer.commands.extend(dynamic_commands)
     
-    #readline.clear_history()
-    readline.set_completer(completer.complete)
+    readline.clear_history()
+    #readline.set_completer(completer.complete)
     readline.parse_and_bind('tab: complete')
-    #readline.set_completer_delims('\t')
+    readline.set_completer_delims('\t')
 
     
     while True:
