c26b6a2989401ecaa712e2d94f21e1fdf556ee41
MU14531453 <michal1442442@gmail.com>
2025-06-18 23:24:34 +0200

    [any message]

--- app/main.py
+++ app/main.py
@@ -136,7 +136,7 @@ def main():
 
     completer.commands = command_list
     #completer.commands = copy(command_list)
-    dynamic_path = subprocess.run('echo $PATH', shell = True, capture_output = True).stdout.decode().split(':')#[1]
+    dynamic_path = [folder for folder in subprocess.run('echo $PATH', shell = True, capture_output = True).stdout.decode().split(':') if folder[:4] == '/tmp']
     print('dp', dynamic_path)
     dynamic_commands = subprocess.run(f'ls -1 {dynamic_path}', shell = True, capture_output = True).stdout.decode()
     dynamic_commands = ''.join(dynamic_commands).strip()
