e298a51c1763151164caa20c66aebb9fed0e428c
MU14531453 <michal1442442@gmail.com>
2025-06-18 22:59:02 +0200

    [any message]

--- app/main.py
+++ app/main.py
@@ -141,7 +141,7 @@ def main():
     
     readline.clear_history()
     readline.set_completer(completer.complete)
-    readline.parse_and_bind('tab: complete')
+    readline.parse_and_bind('tab: possible-completions')
     readline.set_completer_delims('\t')
     
     while True:
