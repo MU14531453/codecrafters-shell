45c193daf8b845923a76e3ec7d373ad365f593b0
MU14531453 <michal1442442@gmail.com>
2025-06-18 23:44:16 +0200

    [any message]

--- app/main.py
+++ app/main.py
@@ -119,8 +119,6 @@ class Autocomplete:
     def complete(self, text, symbol_iter):
         results = [x for x in self.commands if x.startswith(text)] + [None]
         self.results = results
-        if len(results) > 1:
-            return '\a'
         return results[symbol_iter]# + ' '
 
 
@@ -147,7 +145,7 @@ def main():
     
     readline.clear_history()
     readline.set_completer(completer.complete)
-    readline.parse_and_bind('tab: possible-completions')
+    readline.parse_and_bind('tab: complete')#possible-completions')
     readline.set_completer_delims('\t')
     
     while True:
