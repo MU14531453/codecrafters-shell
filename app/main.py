1e1da31552dd608e702ba7a9446b1d424b86b8c9
MU14531453 <michal1442442@gmail.com>
2025-06-23 01:38:45 +0200

    [any message]

--- app/main.py
+++ app/main.py
@@ -118,7 +118,7 @@ class Autocomplete:
     def complete(self, text, symbol_iter):
         results = [x for x in self.commands if x.startswith(text)] + [None]
         self.results = results
-        if len(results) > 1:
+        if len(results) > 2:
             return results[symbol_iter]
         else:
             return results[symbol_iter] + ' '
