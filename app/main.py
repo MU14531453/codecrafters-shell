0a53074b8bf807adbdfdfb229e906b177d6e3a65
MU14531453 <michal1442442@gmail.com>
2025-06-30 00:38:05 +0200

    [any message]

--- app/main.py
+++ app/main.py
@@ -180,7 +180,9 @@ def main():
             command_fork = [c.strip() for c in command_foo.split('|')]
             temp = subprocess.run(command_fork[0], shell = True, capture_output = True).stdout.decode()
             if command_fork[0].strip() == 'tail':
+                print('pretest')
                 print(temp)
+                print('testt')
             if command_fork[0].strip() == 'ls':
                 command = command_fork[1]
             else:
